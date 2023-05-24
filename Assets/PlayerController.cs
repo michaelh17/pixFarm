9ad35af52697510e4c394aed3ef72cee49ffea11
unknown <theodoruskennyvidyanto@gmail.com>
2023-05-24 15:17:09 +0700

    30 minutes in tutorial video

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Vector2 movementInput;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        if(movementInput != Vector2.zero)
+        {
+            rb.Cast()
+        }
+    }
+
+    void Onmove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+    }
+}
